104f47b31b14bde2e48be21a35ad84442dba8461
Leonardo de Freitas Oliveira <leodefoliveira@gmail.com>
2024-07-02 20:06:12 -0300

    feat: app exception handler [T-3]

---
+++ src/main/java/com/br/webshop/purchase/exception/AppExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.br.webshop.purchase.exception;
+
+import com.br.webshop.purchase.dto.ApiErrorResponseDTO;
+import com.br.webshop.purchase.dto.ApiResponseDTO;
+import com.br.webshop.purchase.utils.ApiResponseUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class AppExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public ResponseEntity<ApiErrorResponseDTO> handleException(Exception ex) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ApiResponseUtils.error(ex));
+    }
+}
